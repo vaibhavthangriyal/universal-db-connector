78a2bf26fcb58fa7e8e1d8024c3f763a54c37e31
vaibhavthangriyal <vaibhavthangriyal@gmail.com>
2025-01-30 10:06:12 +0530

    fix

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { createSqlConnection } from "./src/sql";
 import { createMongoConnection } from "./src/noSql";
 
 
-export default {
+export  {
     createSqlConnection,
     createMongoConnection
 }
